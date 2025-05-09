5bf65c4755709527025aded7e82a18596c418e93
Łukasz Monkiewicz <lmonkiewicz@gmail.com>
2025-05-10 00:17:25 +0200

    feat: Initial commit

---
+++ consumer-service/src/test/java/pl/effectivedev/webinar/kafka/guarantee/consumer/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.effectivedev.webinar.kafka.guarantee.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
